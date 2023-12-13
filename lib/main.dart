2908ffbd6bc7df17a7a267a179ee689c8f264e8a
Heck Codes <chandramaulishastri@gmail.com>
2023-12-13 14:25:35 +0530

    Added encryption & home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:dynamic_color/dynamic_color.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:passwordmanager/database/login_credentials.dart';
-
-import 'screens/home_page.dart';
+import 'package:passwordmanager/screens/home_page.dart';
 
 final _defaultLightColorScheme = ColorScheme.fromSwatch(primarySwatch: Colors.purple);
 
@@ -12,6 +14,18 @@ final _defaultDarkColorScheme = ColorScheme.fromSwatch(primarySwatch: Colors.pur
 void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter<LoginCredentials>(LoginCredentialsAdapter());
+
+  const secureStorage = FlutterSecureStorage();
+  final key = await secureStorage.read(key: 'encryption-key');
+  if (key == null) {
+    await secureStorage.write(key: 'encryption-key', value: base64UrlEncode(Hive.generateSecureKey()));
+  }
+
+  final encodedKeyStr = await secureStorage.read(key: 'encryption-key');
+  final encryptionKey = base64Url.decode(encodedKeyStr!);
+
+  await Hive.openBox<LoginCredentials>(loginDetailsBoxName, encryptionCipher: HiveAesCipher(encryptionKey));
+
   runApp(
     DynamicColorBuilder(
       builder: (lightDynamic, darkDynamic) => MaterialApp(
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:passwordmanager/database/login_credentials.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,6 +21,25 @@ class HomePage extends StatelessWidget {
         tooltip: 'Add new login',
         onPressed: () {},
         child: const Icon(Icons.add_rounded),
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: Hive.box<LoginCredentials>(loginDetailsBoxName).listenable(),
+        builder: (context, box, _) {
+          if (box.values.isEmpty) {
+            return const Center(
+              child: Text('No login details present'),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: box.values.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: Text(box.getAt(index)!.name),
+              );
+            },
+          );
+        },
       ),
     );
   }
